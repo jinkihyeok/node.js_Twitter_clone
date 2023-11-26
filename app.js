9bbe841148c783e0066c85d158bd79f93c59e249
jinki <jkhsky121@gmail.com>
2023-11-26 20:55:58 +0900

    Sequelize ORM 적용

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import tweetsRouter from "./router/tweets.js";
 import authRouter from "./router/auth.js";
 import { config } from "./config.js";
 import { initSocket } from "./connection/socket.js";
-import { db, sequelize } from "./db/database.js";
+import { sequelize } from "./db/database.js";
 
 const app = express();
 
@@ -28,8 +28,7 @@ app.use((error, req, res, next) => {
   res.sendStatus(500);
 });
 
-sequelize.sync().then((client) => {
-  // console.log(client);
+sequelize.sync().then(() => {
   const server = app.listen(config.host.port);
   initSocket(server);
 });
